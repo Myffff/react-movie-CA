9bc6f4352f32835e985801e4ead788b3adf1eb9c
Yifei Ma <634042441@qq.com>
2022-11-07 04:04:12 +0800

    add switch for background-color and some other css modify.

--- src/Pages/Series/index.js
+++ src/Pages/Series/index.js
@@ -1,20 +1,61 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Genres from "../../components/Genres/index";
+import useGenre from "../../hooks/useGenre";
+import SingleContent from "../../components/SingleContent/index";
+import CustomPagination from "../../components/pagination/index";
 
 const Series = () => {
   const [genres, setGenres] = useState([]);
   const [selectedGenres, setSelectedGenres] = useState([]);
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPage, setNumOfPage] = useState();
+  const genreforURL = useGenre(selectedGenres);
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=26ba5e77849587dbd7df199727859189&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+    );
+    setContent(data.results);
+    setNumOfPage(data.total_pages);
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchMovies();
+    // eslint-disable-next-line
+  }, [page,genreforURL]);
 
   return (
     <div>
-      <span className="pageTitle">Discover Series</span>
+      <span className="pageTitle">Discover TV Series</span>
       <Genres
         type="tv"
         selectedGenres={selectedGenres}
         setSelectedGenres={setSelectedGenres}
         genres={genres}
         setGenres={setGenres}
+        setPage={setPage}
       />
+      
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="movie"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      {numOfPage > 1 && (
+        <CustomPagination setPage={setPage} numOfPage={numOfPage} />
+      )}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import {createRoot} from "react-dom/client";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import "./App.css";
+import Header from "./components/Header";
+import SimpleBottomNavigation from "./components/MainNav";
+import Movies from "./Pages/Movies/index";
+import Series from "./Pages/Series/index";
+import Trending from "./Pages/Trending";
+import Search from "./Pages/Search/index";
+import { Container } from "@mui/material";
+import MaterialUISwitch from "./components/Switch";
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+const App = () => {
+  const [color, setColor] = useState('#39445a')
+  const [checked, setChecked] = useState(true);
+  const handleChange = (event) => {
+      setChecked(event.target.checked);
+      if (color === '#39445a') {
+        setColor('#cac2d1')
+      } else {
+        setColor('#39445a')
+      }
+  };
+
+  return (
+    <div style={{backgroundColor: color}}>
+    <BrowserRouter>
+      <Header />
+      <FormControlLabel
+          control={<MaterialUISwitch/>}
+          checked={checked}
+          onChange={handleChange}
+          sx={{marginTop: "100px",
+            marginLeft: "20px",
+            }}
+          />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Trending/>} />
+            <Route path="/movies" element={<Movies/>} />
+            <Route path="/series" element={<Series/>} />
+            <Route path="/search" element={<Search/>} />
+            {/* add some more */}
+          </Routes>
+        </Container>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
+    </div>
+  );
+}
 
 const rootElement = createRoot(document.getElementById("root"))
 rootElement.render(<App/>)
